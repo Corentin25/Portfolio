e079fdc36a53d18ee0ed329642ce39ce62117d68
Corentin Busi <corentinbusi@gmail.com>
2026-04-12 15:15:11 +0200

    Intégration de la page Error 404

--- src/Pages/Error404/Error404.jsx
+++ src/Pages/Error404/Error404.jsx
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
 import "./error404.css";
 
 export function Error404() {
   return (
     <main>
-      <div>Error 404</div>
+      <h1 id="Error404">
+        Error <span className="ErrorPhoneView">404</span>
+      </h1>
+      <h2 id="notFound">Oups, cette page n'existe pas !</h2>
+      <Link to={"/home"}>
+        <button className="backButton">Retour</button>
+      </Link>
     </main>
   );
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,6 +5,12 @@ export function Home() {
     <>
       <h1>Intégrateur web à Bourg en Bresse</h1>
       <h2>Votre développeur front-end dans l’Ain</h2>
+      <p className="homeText">
+        Bienvenue sur mon portfolio.<br />
+        Venez découvrir mes projets ainsi que mon
+        évolution professionnelle et plongez avec moi dans l’univers fascinant
+        du développement web !
+      </p>
     </>
   );
 }
